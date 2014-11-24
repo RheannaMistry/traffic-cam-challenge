7f09e54046d10dfb834e65600b9ceb530e3b78e7
Rheanna Mistry <Rhekmist@uw.edu>
2014-11-23 23:08:37 -0800

    Completed

--- js/app.js
+++ js/app.js
@@ -1,13 +1,48 @@
-// List of Seattle Traffic Cameras
-// http://data.seattle.gov/resource/65fc-btcc.json
-
 "use strict";
 
-//put your code here to create the map, fetch the list of traffic cameras
-//and add them as markers on the map
-//when a user clicks on a marker, you should pan the map so that the marker
-//is at the center, and open an InfoWindow that displays the latest camera
-//image
-//you should also write the code to filter the set of markers when the user
-//types a search phrase into the search box
+$(document).ready(function() {
+    var mapOptions = {
+        center: {lat: 47.6, lng: -122.3},
+        zoom: 12
+    };
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    var infoWindow = new google.maps.InfoWindow();
+
+    var cameras;
+    var markers = [];
+
+    $.getJSON('http://data.seattle.gov/resource/65fc-btcc.json', function(data) {
+            cameras = data;
+            data.forEach(function(camera) {
+                var marker = new google.maps.Marker ({
+                    position: {
+                        lat: Number(camera.location.latitude),
+                        lng: Number(camera.location.longitude)
+                    },
+                    map: map
+                });
+                google.maps.event.addListener(marker, 'click', function() {
+                    map.panTo(this.getPosition());
+                    var html = '<p>' + camera.cameralabel + '</p>';
+                    html += '<img src=' + camera.imageurl.url + '>';
+                    infoWindow.setContent(html);
+                    infoWindow.open(map, marker);
+                });
+                $("#search").bind( "search keyup", function() {
+                    var cam = camera.cameralabel.toLowerCase();
+                    var searched = this.value.toLowerCase();
+                    if (cam.indexOf(searched) != -1) {
+                        marker.setMap(map);
+                    } else {
+                        marker.setMap(null);
+                    }
+                })
+            });
+        })
+        .fail(function(error) {
+            alert("Something went wrong!");
+        });
+});
+
+
 
